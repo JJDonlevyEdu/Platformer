b9b2de824bd1017de49439ef31c8c27234c48c56
JJDonlevyEdu <JJDonlevyEdu@github.com>
2018-10-03 16:57:44 +1000

    Added in the start of the collision code
    just started adding in the collision code

--- Platformer/Platformer/Game1.cs
+++ Platformer/Platformer/Game1.cs
@@ -5,6 +5,7 @@ using MonoGame.Extended;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Graphics;
 using MonoGame.Extended.ViewportAdapters;
+using System.Collections;
 
 namespace Platformer
 {
@@ -20,15 +21,42 @@ namespace Platformer
         TiledMap map = null; //Creates an instance of a Tiled map
         TiledMapRenderer mapRenderer = null; // Creates an instance of what makes a Tiled map
 
+        TiledMapTileLayer collisionLayer;
+        public ArrayList allCollisionTiles = new ArrayList();
+        public Sprite[,] levelGrid;
+
+        public int tileHeight = 0;
+        public int levelTileWidth = 0;
+        public int levelTileHeight = 0;
+
+
+        public Rectangle myMap;
+
+        //public Rectangle canvas = new Rectangle();
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+
+            graphics.PreferredBackBufferHeight = 900;
+            graphics.PreferredBackBufferWidth = 1600;
+
             Content.RootDirectory = "Content";
+
+            
         }
-      
+
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            //canvas.X = 0;
+            //canvas.Y = 0;
+            //canvas.Width = graphics.GraphicsDevice.Viewport.Width;
+            //canvas.Height = graphics.GraphicsDevice.Viewport.Height;
+            myMap.X = 0;
+            myMap.Y = 0;
+            myMap.Width = 6400; // each block is 64 by 64 px
+            myMap.Height = 6400;
+
 
             base.Initialize();
         }
@@ -53,12 +81,58 @@ namespace Platformer
             map = Content.Load<TiledMap>("Level1");
             mapRenderer = new TiledMapRenderer(GraphicsDevice);
 
+            SetUpTiles();
         }
        
         protected override void UnloadContent()
         {
         }
-      
+        
+
+        public void SetUpTiles()
+        {
+            tileHeight = map.TileHeight;
+            levelTileHeight = map.Height;
+            levelTileWidth = map.Width;
+            levelGrid = new Sprite[levelTileWidth, levelTileHeight];
+            foreach(TiledMapTileLayer layer in map.TileLayers)
+            {
+                if (layer.Name == "Collision")
+                {
+                    collisionLayer = layer;
+                }
+            }
+
+            int columns = 0;
+            int rows = 0;
+            int loopCount = 0;
+            while (loopCount<collisionLayer.Tiles.Count)
+            {
+                if (collisionLayer.Tiles[loopCount].GlobalIdentifier != 0)
+                {
+                    Sprite tileSprite = new Sprite();
+                    tileSprite.position.X = columns * tileHeight;
+                    tileSprite.position.Y = rows * tileHeight;
+                    tileSprite.width = tileHeight;
+                    tileSprite.height = tileHeight;
+                    tileSprite.UpdateHitBox();
+                    allCollisionTiles.Add(tileSprite);
+                    levelGrid[columns, rows] = tileSprite;
+                }
+
+                columns++;
+
+                if (columns == levelTileWidth)
+                {
+                    columns = 0;
+                    rows++;
+                }
+
+                loopCount++;
+            }
+        }
+
+
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -67,7 +141,7 @@ namespace Platformer
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             player.Update(deltaTime); // Call the 'Update' from our Player class.
 
-            // This makes the camera follow the player's position, it also should center the player to the centr of the camera
+            //This makes the camera follow the player's position, it also should center the player to the centr of the camera
             camera.Position = player.playerSprite.position - new Vector2(graphics.GraphicsDevice.Viewport.Width / 2,
                                                                             graphics.GraphicsDevice.Viewport.Height / 2);
 
@@ -97,5 +171,18 @@ namespace Platformer
 
             base.Draw(gameTime);
         }
+
+        //public float WhereOnMyMap(float maxSize, float positionOfTiles)
+        //{
+        //    float tileSize = 64;
+        //    float position;
+        //    position = positionOfTiles * tileSize;
+
+        //    return position;
+        //}
+        //public float MyMapX(float maxSize, float amountOfTiles, float tileSize)
+        //{
+        //    return 0.0f;
+        //}
     }
 }
